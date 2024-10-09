db63ace29f9b20ab66c3aa94ed0619f8e8eb6568
Kieran Williams <kww26@case.edu>
2024-10-09 18:03:39 -0400

    Submission ready

--- markdown.md
+++ markdown.md
@@ -1,26 +1,28 @@
 This is some regular text
- *This is some regular text*
- **This is some regular text**
-*This is some regular* text
->This is some regular text
 ---
 # This is some heading 1 text
+# #This is some heading 1 text
 ## This is some heading 2 text
 ### This is some heading 3 text
 #This is not heading text
 #### This is not heading text
  # This is not heading text
 ---  
---- This is not a horizontal line
-Below is not a horizontal line
+Below is a horizontal line:
+---
+Below is not a horizontal line:
  ---
- --- This is not a horizontal line
+--- This is not a horizontal line
 ---   
 *This is some italicized text*
 **This is some italicized text* *
 * *This is some italicized text**
+  *This is not italicized text*
+  *This is not italicized* text
 ---
 **This is some bold text**
 ***This is some bold text***
+ **This is not bold text**
 ---
 > This is some block quoted text
+>This is not block quoted text
--- src/MarkdownNode.kt
+++ src/MarkdownNode.kt
@@ -6,30 +6,42 @@ class MarkdownNode(text: String) : Node(text) {
         1   Heading 1
         2   Heading 2
         3   Heading 3
-        4   Bold text
-        5   Italicized text
+        4   Italic
+        5   Bold
         6   Block quote
         7   Horizontal line
      */
 
     init {
+        //On creation, determine the type of Markdown and grab the text from inside the Markdown formatting
         type = parse()
     }
 
+    /**
+     * Formats the text of the MarkdownNode as HTML based on the type of the MarkdownNode
+     *
+     * @return A string in HTML format
+     */
     override fun toHTML(): String {
         when(type) {
             0   ->  {   return  "<p>$text</p>"    }
             1   ->  {   return  "<h1>$text</h1>"    }
             2   ->  {   return  "<h2>$text</h2>"    }
             3   ->  {   return  "<h3>$text</h3>"    }
-            4   ->  {   return  "<p><b>$text</b></p>"  }
-            5   ->  {   return  "<p><i>$text</i></p>"  }
+            4   ->  {   return  "<p><i>$text</i></p>"  }
+            5   ->  {   return  "<p><b>$text</b></p>"  }
             6   ->  {   return  "<blockquote>$text</blockquote>"    }
             7   ->  {   return  "<hr>"  }
         }
         return ""
     }
 
+    /**
+     * Uses regex to match Markdown formatting and determine, and strips the formatting
+     * This method changes the MarkdownNode's text property
+     *
+     * @return The type code of the Markdown formatting
+     */
     private fun parse() : Int {
 
         val heading = Regex("^(#{1,3})\\s(.*)")  // Matches with 1-3 leading '#'s, a whitespace, and anything after
@@ -44,21 +56,26 @@ class MarkdownNode(text: String) : Node(text) {
         val horizontal = Regex("^---\\s*\$")    //Matches a leading '---', and a trailing whitespace or nothing
 
 
-        fun multiParser(matchGroups: MatchGroupCollection) : Int {
-            val level = matchResult.groups[1].value.length //Asserting non-null guaranteed by regex.matches(text) == true
-            text = results.groups[2]!!.value
+        /**
+         * Combines some operations that both the asterisk and heading regex matches need
+         *
+         * @param matchGroups The groups returned by a regex.matchEntire(String). Assumed to have 2 groups
+         * @return The number of characters in the first group ('*'s or '#'s) (Is only ever 1, 2, or 3)
+         */
+        fun multiParser(matchGroups: MatchGroupCollection) : Int { //Used for the repetition dependent formats (Headings, Italics, Bolds)
+            val level = matchGroups[1]!!.value.length //Asserting non-null guaranteed by regex.matches(text) == true
+            text = matchGroups[2]!!.value
             return level
         }
 
         //Heading
         if (heading.matches(text)) {
-            return multiParser(heading.matchEntire(text)!!.groups[1:2])
+            return multiParser(heading.matchEntire(text)!!.groups) //Asserting non-null guaranteed by heading.matches(text) == true
         }
 
         //Bold and Italic
         if (asterisk.matches(text)) {
-            val results = asterisk.matchEntire(text)
-            val asteriskLevel = results!!.groups[1]!!.value.length //Asserting non-null guaranteed by asterisk.matches(text) == true
+            return multiParser(asterisk.matchEntire(text)!!.groups) + 3 //Asserting non-null guaranteed by asterisk.matches(text) == true
         }
 
         //Block Quote
